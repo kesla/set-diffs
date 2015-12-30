06759cf3216a5dc00f16dd4d3785ec7c9eb68070
David Björklund <david.bjorklund@gmail.com>
2015-12-30 23:32:10 +0100

    optimize

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ var arrayFrom = require('array-from');
 var Set = require('es6-set');
 
 module.exports = function (a, b) {
-  var keep = new Set(
-    arrayFrom(a).filter(function (x) { return b.has(x); })
-  );
-  var remove = new Set(
-    arrayFrom(a).filter(function (x) { return !keep.has(x); })
-  );
-  var add = new Set(
-    arrayFrom(b).filter(function (x) { return !keep.has(x); })
-  );
+  var keep = new Set();
+  var remove = new Set();
+  var add = new Set();
+
+  a.forEach(function (value) {
+    if (b.has(value)) {
+      keep.add(value);
+    } else {
+      remove.add(value);
+    }
+  });
+
+  b.forEach(function (value) {
+    if (!keep.has(value)) {
+      add.add(value);
+    }
+  });
+
   return {
     keep: keep,
     remove: remove,
