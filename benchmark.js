056db19fa0b5701cacc79e7793773a380de2dcad
David Björklund <david.bjorklund@gmail.com>
2015-12-30 23:52:42 +0100

    More benchmark

--- benchmark.js
+++ benchmark.js
@@ -4,11 +4,25 @@ var range = require('fill-range');
 var Set = require('es6-set');
 var diff = require('./index');
 
-var set1 = new Set(range(1, 2000));
-var set2 = new Set(range(1000, 3000));
+var set1 = new Set(range(0, 1999));
+var set2 = new Set(range(1000, 2999));
+var set3 = new Set(range(2000, 3999));
+var i;
 
-console.time('benchmark');
-for (var i = 0; i < 100; ++i) {
+console.time('no overlap');
+for (i = 0; i < 100; ++i) {
+  diff(set1, set3);
+}
+console.timeEnd('no overlap');
+
+console.time('some overlap');
+for (i = 0; i < 100; ++i) {
   diff(set1, set2);
 }
-console.timeEnd('benchmark');
+console.timeEnd('some overlap');
+
+console.time('all overlap');
+for (i = 0; i < 100; ++i) {
+  diff(set1, set1);
+}
+console.timeEnd('all overlap');
